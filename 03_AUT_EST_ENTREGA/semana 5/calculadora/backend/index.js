c6f5d30753bc5e767fbacb1c1cd99114ffba6e18
Cristiane de Andrade Coutinho <cristiane.coutinho@sou.inteli.edu.br>
2022-05-24 14:29:59 -0300

    cauculadora

--- 03_AUT_EST_ENTREGA/semana 5/calculadora/backend/index.js
+++ 03_AUT_EST_ENTREGA/semana 5/calculadora/backend/index.js
@@ -1,28 +1,30 @@
 //criado uma função, oper, quando a seleção da contar for escolhida inicia o bloco de codigo
 function calcular(oper) {
-    //são colocadas nas variaveis os valores pegado no formulario, não foi utilizado .documentById nem QuerySelector
-    var valor1 = document.calculadora.valor1.value;
-    var valor2 = document.calculadora.valor2.value;
- 
-    if (oper == "somar") {
-       var res = parseInt(valor1) + parseInt(valor2);
-       //A função parseInt() analisa um argumento string e retorna um inteiro na base especificada.
-       //primeira operação da lista
-    } 
-    else {
-       if (oper == "subtrair") {
-          var res = valor1-valor2;
-       } 
-       else {
-          if (oper == "multiplicar") {
-             var res = valor1*valor2;
-          }
-         else {
-             var res = valor1/valor2;
-          }
-       }
-    }
- 
-    document.calculadora.res.value = res;
-    //nessa função res será o resultado da função, não o res de requisição
- }
+   //são colocadas nas variaveis os valores pegado no formulario, não foi utilizado .documentById nem QuerySelector
+   var valor1 = document.calculadora.valor1.value;
+   var valor2 = document.calculadora.valor2.value;
+
+   switch (oper) {
+      case "somar":
+         var res = parseInt(valor1) + parseInt(valor2);
+         break;
+      case "subtrair":
+         var res =  parseInt(valor1) -  parseInt(valor2);
+         break;
+      case "multiplicar":
+         var res =  parseInt(valor1) *  parseInt(valor2);
+         break;
+      case "dividir":
+         var res =  parseInt(valor1) /  parseInt(valor2);
+         break;
+      case "dividirInt":
+         var res = Math.trunc ( parseInt(valor1) /  parseInt(valor2));
+         break;
+      case "dividirRest":
+         var res =  parseInt(valor1) % parseInt(valor2);
+         break;
+   }
+
+   document.calculadora.res.value = res;
+   //nessa função res será o resultado da função, não o res de requisição
+}
