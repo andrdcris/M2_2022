6e6e128db6b539ac44a791675f8f95d319986bbc
Cristiane de Andrade Coutinho <cristiane.coutinho@sou.inteli.edu.br>
2022-06-02 23:15:33 -0300

    tutorial etapa 6

---
+++ aula/app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3011;
+const server = http.createServer((req, res) => {
+})
+
+app.get ("/bem", function (req, res){
+res.send("Bem vindo");
+})
