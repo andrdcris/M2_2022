8499ef061c807e6a989b9cd1c163af8013897f9b
Cristiane de Andrade Coutinho <cristiane.coutinho@sou.inteli.edu.br>
2022-06-09 15:31:02 -0300

    Exercicio semana 7

--- 04_AUT_EST_EX_OBRIGATORIOS/semana 7/exercicio 3/script.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/semana 7/exercicio 3/script.js
@@ -1,9 +1,14 @@
-function enviar(){
-    var number = document.querySelector("#number")
-
-    if (number.value == ""){
-        alert("Informe um número de telefone")
-    }
-
-
-}
+$(document).ready(function(){
+    $("#enviar").on("click", function(){
+        var telefone = $("#number").val();
+        var parte1 = telefone.slice(4,9);
+        var parte2 = telefone.slice(9,10);
+        var parte3 = telefone.slice(10,16);
+        
+        if((telefone[0] == "(") && (telefone [3] ==")") && (parte2 == "-") && (parte1.length == 5) && (parte3.length == 4)){
+            alert("Telefone correto");
+        } else{
+            alert("Formato de telefone incorreto!");
+        }
+    })
+})
--- 04_AUT_EST_EX_OBRIGATORIOS/semana 7/exercicio 4/script.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/semana 7/exercicio 4/script.js
@@ -1,21 +1,21 @@
 $('#btn').on("click", function () {
     var pessoas = parseInt($("#pessoas").val());
-    var tipo = parseInt($("#tipo").val());
-
-    if(tipo == "diurno" && pessoas >= 50){
-        var total = pessoas*200
-        var res = total * 0.40
-        $("#resultado").val(res)
-        console.log(res)
-    }else{
-        $("#resultado").val(total)
+    var tipo = $("#tipo").val();
+    var totalN = pessoas * 200;
+    var totalD = pessoas * 100;
+    if (tipo == "noturno" && pessoas >= 50) {
+        var desc = totalN * 0.40;
+        var res = totalN - desc;
+        $("#resultado").html(res);
+    } else if (tipo == "noturno" && pessoas <= 50) {
+        $("#resultado").html(totalN);
     }
 
-    if (tipo == "noturno" && pessoas >= 50) {
-        var total = pessoas*100
-        var res = total * 0.40
-        $("#resultado").val(res)
-    } else {
-        $("#resultado").val(total)
+    if (tipo == "diurno" && pessoas >= 50) {
+        var res = totalD * 0.40;
+        var res = totalD - desc;
+        $("#resultado").html(res);
+    } else if (tipo == "diurno" && pessoas <= 50) {
+        $("#resultado").html(totalD);
     }
 })
