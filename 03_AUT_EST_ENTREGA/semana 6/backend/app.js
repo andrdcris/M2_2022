afb43b89d517f5ba17824641344c64cc821e42a0
Cristiane de Andrade Coutinho <cristiane.coutinho@sou.inteli.edu.br>
2022-06-14 15:21:56 -0300

    Exercicio de personalização semana 6

--- 03_AUT_EST_ENTREGA/semana 6/backend/app.js
+++ 03_AUT_EST_ENTREGA/semana 6/backend/app.js
@@ -1,49 +1,38 @@
-const express = require('express'); 
-const app = express();
+const express = require("express"); 
+const app = express(); 
+
 const hostname = '127.0.0.1';
 const port = 3071;
 const sqlite3 = require('sqlite3').verbose(); 
 const DBPATH = 'db.db'; 
+
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.use(express.static("../frontend"));
+
 app.use(express.json());
 
-//requisição get etapa 5
-app.get('/experiencia', (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Access-Control-Allow-Origin', '*'); 
+app.get('/users', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
 
-  var db = new sqlite3.Database(DBPATH); 
-var sql = 'SELECT * FROM experiencia ORDER BY instituicao COLLATE NOCASE';
-  db.all(sql, [],  (err, rows ) => {
-      if (err) {
-          throw err;
-      }
-      res.json(rows);
-  });
-  db.close(); 
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+  var sql = 'SELECT * FROM experiencia ORDER BY titulo COLLATE NOCASE';
+	db.all(sql, [],  (err, rows ) => {
+		if (err) {
+		    throw err;
+		}
+		res.json(rows);
+	});
+	db.close(); // Fecha o banco
 });
 
 
-//inseir experiencia
-app.post('/instituicaoinsert', urlencodedParser, (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
 
-  sql = "INSERT INTO experiencia (instituicao, id) VALUES ('" + req.body.title + "', 33,)";
-  var db = new sqlite3.Database(DBPATH); // Abre o banco
-  db.run(sql, [],  err => {
-      if (err) {
-          throw err;
-      }
-  });
-  db.close(); // Fecha o banco
-  res.end();
-});
-
-
+//servidor iniciado
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
+
+
