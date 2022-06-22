4f70eff64de91ec6be717538f41c825ae8204c9a
Cristiane de Andrade Coutinho <cristiane.coutinho@sou.inteli.edu.br>
2022-06-21 21:44:50 -0300

    Curriculo semana 7

--- 03_AUT_EST_ENTREGA/semana 7/backend/app.js
+++ 03_AUT_EST_ENTREGA/semana 7/backend/app.js
@@ -13,7 +13,7 @@ app.use(express.static("../frontend"));
 
 app.use(express.json());
 
-app.get('/users', (req, res) => {
+app.get('/getContato', (req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
 
@@ -28,6 +28,69 @@ app.get('/users', (req, res) => {
 	db.close(); // Fecha o banco
 });
 
+//CRUD
+app.get('/getHabilidade', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	var sql = 'SELECT * FROM HABILIDADES';
+	db.all(sql, [], (err, rows) => {
+		if (err) {
+			throw err;
+		}
+		res.json(rows);
+	});
+	db.close(); // Fecha o banco
+});
+
+// Insere um registro (é o C do CRUD - Create)
+app.post('/insertHabilidade', urlencodedParser, (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+    sql = "INSERT INTO HABILIDADES (habilidade) VALUES ('" + req.body.habilidade + "')";
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    db.run(sql, [],  err => {
+        if (err) {
+            throw err;
+        }
+    });
+    db.close(); // Fecha o banco
+    res.end();
+});
+
+// Atualiza um registro (é o U do CRUD - Update)
+app.post('/updateHabilidade', urlencodedParser, (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+    sql = "UPDATE HABILIDADES SET habilidade = '" + req.body.habilidade + "' WHERE id = " + req.body.id;
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    db.run(sql, [],  err => {
+        if (err) {
+            throw err;
+        }
+        res.end();
+    });
+    db.close(); // Fecha o banco
+});
+
+// Exclui um registro (é o D do CRUD - Delete)
+app.post('/deleteHabilidade', urlencodedParser, (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+    sql = "DELETE FROM HABILIDADES WHERE id = " + req.body.id;
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    db.run(sql, [],  err => {
+        if (err) {
+            throw err;
+        }
+        res.end();
+    });
+    db.close(); // Fecha o banco
+});
 
 
 //servidor iniciado
--- 03_AUT_EST_ENTREGA/semana 7/frontend/script.js
+++ 03_AUT_EST_ENTREGA/semana 7/frontend/script.js
@@ -1,7 +1,21 @@
+window.onload = function(){
+        getContato();
+}
 function botao(){
     $('.bio').append("<h3>Sou Cristiane, brasiliense e apaixonada por inovação e tecnologia</h3>")
 }
 
 function teste() {
     console.log("teste")
+}
+function getContato(){
+    var url = "http://127.0.0.1:3071/getContato";
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", url, false);
+    xhttp.send();//A execução do script pára aqui até a requisição retornar do servidor
+var result = JSON.parse(xhttp.response);
+document.getElementById("telefone").innerHTML = result[0].telefone;
+document.getElementById("email").innerHTML = result[0].email;
+    //console.log(xhttp.responseText);
 }
